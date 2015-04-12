f560dbf4634460cc5df1b74a9c6837852b3d2050
loxxo <water2ice@gmail.com>
2015-04-13 05:12:16 +0800

    organised path

--- Youlv/TopTabBarController.swift
+++ Youlv/TopTabBarController.swift
@@ -8,9 +8,39 @@
 
 import UIKit
 
-class TopTabBarController:
-.
+class TopTabBarController: UITabBarController {
 
-{
-   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reLocTabBar();
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func reLocTabBar()
+    {
+
+        if self.navigationController != nil
+        {
+            var y =  self.navigationController?.navigationBar.frame.size.height;
+            tabBar.frame = CGRectMake(0,y!,tabBar.frame.size.width,tabBar.frame.size.height);
+            
+        }
+     }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
