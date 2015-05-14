cc5241201ca0dddcbf144435321c4b142bb38524
loxxo <water2ice@gmail.com>
2015-05-14 21:33:41 +0800

    Discovery

---
+++ Youlv/Controllers/Discovery/JobDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JobDetailViewController.swift
+//  Youlv
+//
+//  Created by Lost on 14/05/2015.
+//  Copyright (c) 2015 Ramy. All rights reserved.
+//
+
+import UIKit
+
+class JobDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
