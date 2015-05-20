69d7af2b09e6d143d995a409f810c8388997d580
loxxo <water2ice@gmail.com>
2015-05-21 04:37:06 +0800

    IM Model Added

---
+++ Youlv/Classes/AvatarImageView.swift
@@ -0,0 +1,13 @@
+//
+//  AvatarImageView.swift
+//  Youlv
+//
+//  Created by Lost on 21/05/2015.
+//  Copyright (c) 2015 Ramy. All rights reserved.
+//
+
+import Cocoa
+
+class AvatarImageView: UIImageView {
+
+}
---
+++ Youlv/Controllers/Discovery/NewTopicReplyViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewTopicReplyViewController.swift
+//  Youlv
+//
+//  Created by Lost on 20/05/2015.
+//  Copyright (c) 2015 Ramy. All rights reserved.
+//
+
+import Cocoa
+
+class NewTopicReplyViewController: UIViewController {
+
+}
