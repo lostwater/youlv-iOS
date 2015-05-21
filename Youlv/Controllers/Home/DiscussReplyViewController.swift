99f0888864ac0d3cde4491dd67edb09e3a7751b3
loxxo <water2ice@gmail.com>
2015-05-22 01:19:09 +0800

    功能上目前已知问题：发布选项的黑白名单，头像跳转 用户注册成功后自动在环信注册，登陆后也登陆环信，两边都创建群聊，密码只在友律服务器，好友只在友律服务器

---
+++ Youlv/Controllers/Home/DiscussReplyViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DiscussReplyViewController.swift
+//  Youlv
+//
+//  Created by Lost on 21/05/2015.
+//  Copyright (c) 2015 Ramy. All rights reserved.
+//
+
+import Cocoa
+
+class DiscussReplyViewController: UIViewController {
+
+}
---
+++ Youlv/IMController/PrivateChatViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PrivateChatViewController.swift
+//  Youlv
+//
+//  Created by Lost on 22/05/2015.
+//  Copyright (c) 2015 Ramy. All rights reserved.
+//
+
+import Cocoa
+
+class PrivateChatViewController: ChatViewController {
+
+}
